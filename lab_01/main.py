0c9213c8e7c98f8afab7773ec8bdc46c8afc6799
Ivan Spasenov <spasenovivan@mail.ru>
2017-03-03 16:55:43 +0300

    Add new test function(Test2) in the programm

--- lab_01/main.py
+++ lab_01/main.py
@@ -12,11 +12,24 @@ def Test1():
   return Arr, 5
 
 
+def Test2():
+  Arr = list()
+  
+  Arr.append(5)
+  Arr.append(5)
+  Arr.append(6)
+  Arr.append(6)
+  Arr.append(6)
+  
+  return Arr, 5
+
+
 def main():
   Arr, N = Test1()
-  
+    
   print("Maximal value is found " + str(GetMaxCount(Arr, N)) + " times.")
-
+  Arr, N = Test2()
+  print("Maximal value is found " + str(GetMaxCount(Arr, N)) + " times.")
 
 if __name__ == '__main__':
   main()
