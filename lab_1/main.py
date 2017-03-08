1d355d42bcac93868da57295f1b07b0e0ff65108
Ivan Spasenov <spasenovivan@mail.ru>
2017-03-08 22:16:33 +0300

    Add new test which show that programm not working

--- lab_1/main.py
+++ lab_1/main.py
@@ -11,6 +11,17 @@ def test_1():
   
   return arr, 5
 
+def test_2():
+  arr = list()
+  
+  arr.append(0)
+  arr.append(-1)
+  arr.append(3)
+  arr.append(-2)
+  arr.append(-2)
+  
+  return arr, 5
+
 def main():
   arr, n = test_1()
 
@@ -21,7 +32,14 @@ def main():
 
   print("Result")
   print_array(new_arr, new_n)
+  
+  arr, n = test_2()
+  print("Source")
+  print_array(arr, n)
+  new_arr, new_n = form_array(arr, n)
 
+  print("Result which show that programm not working")
+  print_array(new_arr, new_n)
 
 if __name__ == '__main__':
   main()
