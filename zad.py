357431c53b92fd1f56195df9fc8fb3dec256ad9a
U-VLAB\user-lab01 <user-lab01@LAB-01-57.vlab.bmstu.ru>
2017-05-06 13:40:42 +0300

    changed zad

--- zad.py
+++ zad.py
@@ -1,38 +1,89 @@
-# Не успел доделать, она не работает
-s1 = input("Введите имя входного файла")
-s2 = input("Введите имя выходного файла")
-f1 = open(s1,'r')
 
-while True:
+def find_Min_Max(a,k):
+    maxi = a[0]
+    mini = a[0]
+    for i in range(k):
+        if len(a[i])>len(maxi):
+            maxi = a[i]
+        if len(a[i])<len(mini):
+            mini = a[i]
+    return mini, maxi
+
+def swap(a,maxi,mini,k):
+    ss = ''
+    for i in range(k): 
+        if a[i] == maxi:
+            ss += mini+' '
+        elif a[i] == mini:
+            ss += maxi+' '   
+        else:
+            ss += a[i]+' '
+    return ss
+
+def readString(s):
+    kk = 0
     chis = [0]*100
     k = 0
-    s = f1.readline()
+
     if s == '':
-        break
+        return -1, -1
+    s = s.rstrip()
     s += ' '
     ch = ''
-
     for i in range(len(s)):
         if s[i] != ' ':
             ch += s[i]
         else:
             chis[k] = ch
+            kk += 1
             ch = ''
             k += 1
-    maxi = chis[0]
-    mini = chis[0]
-    for i in range(k):
-        if len(chis[i])>len(maxi):
-            maxi = chis[i]
-        if len(chis[i])<len(mini):
-            mini = chis[i]
+    if kk > 100:
+        return -2,-2
+    return chis, k
+
+def printString(s,s2):
     f2 = open(s2,'a+')
-    for i in range(k):
-        if chis[i] == maxi:
-            f2.write(mini+' ')
-        elif chis[i] == mini:
-            f2.write(maxi+' ')
-        else:
-            f2.write(chis[i]+' ')
+    f2.write(s+'\n')
     f2.close()
+def check(s1,s2):
+    
+    try:
+        f1 = open(s1,'r')
+    except IOError:
+        print ("Не найден входной файл")
         
+    
+    try:
+        f2 = open(s2,'w')
+    except IOError:
+        print ("Не найден выходной файл")
+        return -2
+    f1.close()
+    f2.close()
+    return 0
+    
+def main():
+    kk = 0
+    s1 = input("Введите имя входного файла: ")
+    s2 = input("Введите имя выходного файла: ")
+    t = check(s1,s2)
+    if t == 0:
+        f1 = open(s1,'r')
+        while True:
+            s = f1.readline()
+            kk += 1
+            if kk > 100:
+                break
+            chis,k = readString(s)
+            if k == -1:
+                break
+            if k == -2:
+                print("Ошибка слов в строке больше 100")
+                break
+            mini,maxi = find_Min_Max(chis,k)
+            ss = swap(chis,maxi,mini,k)
+            printString(ss,s2)
+        f1.close()
+
+main()
