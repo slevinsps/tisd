c403b9685ffea33179058f5a7a46a90d32777324
U-VLAB\user-lab01 <user-lab01@LAB-01-11.vlab.bmstu.ru>
2017-05-20 11:47:58 +0300

    add check

--- homework1/zad1_7.py
+++ homework1/zad1_7.py
@@ -10,27 +10,29 @@ def ReadMatrix():
     n = int(input("Введите размер матрицы: "))  
     try:
         if n > 100:
-            return (-1,-1,-1)  # Превышена размерность матрицы
+            return (-1,None,-1)  # Превышена размерность матрицы
         elif n < 1:
-            return (-2,-2,-2)  # Некорректный размер матрицы
+            return (-2,None,-1)  # Некорректный размер матрицы
         print("Введите матрицу:")
         a = [[]]*n
         for i in range(n):
             s = input()
             x = s.split()
             if len(x) != n:
-                return (-3,-3,-3)  # Матрица введена некорректно
+                return (-3,None,-1)  # Элементов матрицы в строке больше, чем ожидалось
             for j in range(len(x)):
                 x[j] = int(x[j])
             a[i] = x
     except (TypeError, ValueError):
-            return (-3,-3,-3)  # Матрица введена некорректно
+            return (-4,None,-1)  # Элементы матрицы введены некорректно
     return (0,a,n)
 
 def OpredPoboch(a,n):
     assert (1 <= n <= 100),"Некорректный размер матрицы"
-    poboch = [0] * (2*n-1) 
-    for i in range(2*n-1): 
+    assert (a != None),"Матрица не инициализирована"
+    kolvo_diag = 2*n-1
+    poboch = [0] * (kolvo_diag) 
+    for i in range(kolvo_diag): 
         poboch[i] = [0] * n
     for i in range(n):
         for j in range(n):
@@ -40,8 +42,10 @@ def OpredPoboch(a,n):
 
 def OpredGlav(a,n):
     assert (1 <= n <= 100),"Некорректный размер матрицы"
-    glav = [0] * (2*n-1) 
-    for i in range(2*n-1): 
+    assert (a != None),"Матрица не инициализирована"
+    kolvo_diag = 2*n-1
+    glav = [0] * (kolvo_diag) 
+    for i in range(kolvo_diag): 
         glav[i] = [0] * n
     for i in range(n):
         for j in range(n):
@@ -50,8 +54,10 @@ def OpredGlav(a,n):
 
 def ProizvOtr(poboch,n):
     assert (1 <= n <= 100),"Некорректный размер матрицы"
-    pr = [1]*(2*n-1)
-    for i in range(2*n-1):
+    assert (poboch != None),"Массив не инициализирован"
+    kolvo_diag = 2*n-1
+    pr = [1]*(kolvo_diag)
+    for i in range(kolvo_diag):
         bool = False
         for j in range(n):
             if poboch[i][j] < 0:
@@ -63,8 +69,10 @@ def ProizvOtr(poboch,n):
                 
 def SumPoloj(glav,n):
     assert (1 <= n <= 100),"Некорректный размер матрицы"
-    summa = [0]*(2*n-1)
-    for i in range(2*n-1):
+    assert (glav != None),"Массив не инициализирован"
+    kolvo_diag = 2*n-1
+    summa = [0]*(kolvo_diag)
+    for i in range(kolvo_diag):
         bool = False
         for j in range(n):
             if glav[i][j] > 0:
@@ -76,9 +84,11 @@ def SumPoloj(glav,n):
 
 def Min(pr,n):
     assert (1 <= n <= 100),"Некорректный размер матрицы"
+    assert (pr != None),"Массив не инициализирован"
+    kolvo_diag = 2*n-1
     min_pr = 0
     mini = -1
-    for i in range(2*n-1):
+    for i in range(kolvo_diag):
         if pr[i] != 0:
             if min_pr != 0:
                 if pr[i] < min_pr:
@@ -87,14 +97,16 @@ def Min(pr,n):
             else:
                 min_pr = pr[i]
                 mini = i
-    assert (-1 <= mini < 2*n-1),"Некорректный индекс минимального элемента"
+    assert (-1 <= mini < kolvo_diag),"Некорректный индекс минимального элемента"
     return mini
 
 def Max(summa,n):
     assert (1 <= n <= 100),"Некорректный размер матрицы"
+    assert (summa != None),"Массив не инициализирован"
+    kolvo_diag = 2*n-1
     max_summa = -1
     maxi = -1
-    for i in range(2*n-1):
+    for i in range(kolvo_diag):
         if summa[i] != -1:
             if max_summa != -1:
                 if summa[i] > max_summa:
@@ -103,12 +115,14 @@ def Max(summa,n):
             else:
                 max_summa = summa[i]
                 maxi = i
-    assert (-1 <= maxi < 2*n-1),"Некорректный индекс максимального элемента"
+    assert (-1 <= maxi < kolvo_diag),"Некорректный индекс максимального элемента"
     return maxi
 
 def form_diag(arr,ind,n):
-    assert (0 <= ind < 2*n-1),"Некорректный индекс элемента"
+    kolvo_diag = 2*n-1
+    assert (0 <= ind < kolvo_diag),"Некорректный индекс элемента"
     assert (1 <= n <= 100),"Некорректный размер матрицы"
+    assert (arr != None),"Массив не инициализирован"
     if ind <= (2*n-1)//2:
         ans = [0]*(ind+1)
         for i in range(ind+1):
@@ -116,14 +130,16 @@ def form_diag(arr,ind,n):
     else:
         ans = [0]*(2*n-ind-1)
         k = 0
-        for i in range(ind-(2*n-1)//2,n):
+        for i in range(ind-(kolvo_diag)//2,n):
             ans[k] = arr[ind][i]
             k += 1    
     return ans
 
 def print_poboch_giagonal(min_index,poboch_diagonali,n):
+    kolvo_diag = 2*n-1
     assert (1 <= n <= 100),"Некорректный размер матрицы"
-    assert (-1 <= min_index < 2*n-1),"Некорректный индекс минимального элемента"
+    assert (-1 <= min_index < kolvo_diag),"Некорректный индекс минимального элемента"
+    assert (poboch_diagonali != None),"Массив не инициализирован"
     if min_index < 0:
         print("Невозможно найти диагональ, параллельную побочной имеющую минимальное\
                произведение отрицательных элементов, так как их нет в матрице")
@@ -134,8 +150,10 @@ def print_poboch_giagonal(min_index,poboch_diagonali,n):
         print(" - диагональ, параллельная побочной имеющая минимальное произведение отрицательных элементов")
         
 def print_glav_giagonal(max_index,glav_diagonali,n):
+    kolvo_diag = 2*n-1
     assert (1 <= n <= 100),"Некорректный размер матрицы"
-    assert (-1 <= max_index < 2*n-1),"Некорректный индекс максимального элемента"
+    assert (-1 <= max_index < kolvo_diag),"Некорректный индекс максимального элемента"
+    assert (glav_diagonali != None),"Массив не инициализирован"
     if max_index < 0:
         print("Невозможно найти диагональ, параллельную главной имеющую максимальную\
               сумму положительных элементов, так как их нет в матрице")
@@ -146,12 +164,15 @@ def print_glav_giagonal(max_index,glav_diagonali,n):
         print(" - диагональ, параллельная главной имеющая максимальную сумму положительных элементов ")
 
 def error_print(k):
+    assert (k != None),"Код ошибки не инициализирован"
     if k == -1:
         print("Превышена размерность матрицы")
     elif k == -2:
         print("Некорректный размер матрицы")
     elif k == -3:
-        print("Матрица введена некорректно")
+        print("Элементов матрицы в строке больше, чем ожидалось")
+    elif k == -4:
+        print("Элементы матрицы введены некорректно")
 
 def main():
     try:
